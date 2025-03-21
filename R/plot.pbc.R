f0a90578ed3e0ddcfd32ecc456e79652750ef49c
Jacob Anhøj <jacob@anhoej.net>
2025-03-21 09:30:40 +0100

    Remove tick labels from inner plots.

--- R/plot.pbc.R
+++ R/plot.pbc.R
@@ -47,6 +47,8 @@ plot.pbc <- function(x, ...) {
                      floor(sqrt(n_facets)),
                      x$ncol)
   n_rows   <- ceiling(n_facets / n_cols)
+  outer_y <- seq(1, n_facets, by = n_cols)
+  outer_x <- seq(n_facets - n_cols + 1, n_facets)
 
   # Set colours and graphical parameters.
   col1 <- 'steelblue'
@@ -56,13 +58,16 @@ plot.pbc <- function(x, ...) {
 
   cex.adj <- 0.9
 
-  op <- graphics::par(mfrow    = c(n_rows, n_cols),
-                      mar      = c(3, 2, ifelse(n_facets == 1, 0, 2), 3),
-                      oma      = c(2, 2.6, ifelse(is.null(x$title), 1, 3.5), 0),
-                      cex      = cex.adj,
-                      cex.axis = cex.adj,
-                      cex.main = cex.adj,
-                      cex.lab  = cex.adj)
+  op <- graphics::par(
+    mfrow    = c(n_rows, n_cols),
+    mar      = c(1.5, 1.2, ifelse(n_facets == 1, 0, 2), 3),
+    oma      = c(2.5, 4.1, ifelse(is.null(x$title), 1, 2.6), 0),
+    cex      = cex.adj,
+    cex.axis = cex.adj,
+    cex.main = cex.adj,
+    cex.lab  = cex.adj
+  )
+
   on.exit(graphics::par(op))
 
   axis_par <- list(las       = 1,
@@ -74,7 +79,9 @@ plot.pbc <- function(x, ...) {
 
   # Begin draw facets ----------------------------------------------------------
   d <- split(d, d$facet)
+  j <- 0
   for(i in d) {
+    j <- j + 1
     # Set colours and centre line types.
     dotcol                 <- ifelse(i$useful, col1, col4)
     dotcol[i$sigma.signal] <- col3
@@ -110,35 +117,45 @@ plot.pbc <- function(x, ...) {
 
     # Add box and axes.
     graphics::box(bty = 'l', col = col2)
-    do.call(x_class, c(1, axis_par))          # x axis
-    do.call(graphics::axis, c(2, axis_par))   # y axis
+
+    do.call(x_class,                            # x axis
+            c(1, axis_par,
+              labels = ifelse(j %in% outer_x,
+                              TRUE,
+                              FALSE)))
+
+    do.call(graphics::axis,                     # y axis
+            c(2, axis_par,
+              labels = ifelse(j %in% outer_y | !yfixed,
+                              TRUE,
+                              FALSE)))
 
     # Add lines and points.
-    graphics::lines(i$x[base], i$cl[base],     # centre line
+    graphics::lines(i$x[base], i$cl[base],      # centre line
                     col = clcol,
                     lty = cltyp)
     graphics::lines(i$x[-base], i$cl[-base],
                     col = clcol2,
                     lty = cltyp2)
-    graphics::lines(i$x[base], i$lcl[base],    # lower control limit
+    graphics::lines(i$x[base], i$lcl[base],     # lower control limit
                     col = col2)
     graphics::lines(i$x[-base], i$lcl[-base],
                     col = col2)
-    graphics::lines(i$x[base], i$ucl[base],    # upper control limit
+    graphics::lines(i$x[base], i$ucl[base],     # upper control limit
                     col = col2)
     graphics::lines(i$x[-base], i$ucl[-base],
                     col = col2)
-    graphics::lines(i$x, i$y,                  # data line
+    graphics::lines(i$x, i$y,                   # data line
                     col = col1,
                     lwd = 2.5)
-    graphics::points(i$x, i$y,                 # data points
+    graphics::points(i$x, i$y,                  # data points
                      cex = 0.8,
                      col = dotcol,
                      pch = 19)
 
     # Add part labels.
     if (!is.null(freeze)) {
-      graphics::mtext(x$partlabs[1],                               # phase 1
+      graphics::mtext(x$partlabs[1],                                # phase 1
                       at   = mean(c(as.numeric(i$x[1]),
                                     as.numeric(i$x[x$freeze]))),
                       cex  = 0.7,
@@ -154,7 +171,7 @@ plot.pbc <- function(x, ...) {
     }
 
     # Add centre line label.
-    graphics::mtext(formatC(i$cl[1]),
+    graphics::mtext(formatC(i$cl[1], digits = 2, format = 'fg'),
                     side = 4,
                     at   = i$cl[1],
                     adj  = 0,
@@ -173,12 +190,12 @@ plot.pbc <- function(x, ...) {
   # Begin finish plot ----------------------------------------------------------
   graphics::mtext(x$xlab,           # x axis label
                   side  = 1,
-                  line  = 0.5,
+                  line  = 1.3,
                   cex   = cex.adj,
                   outer = TRUE)
   graphics::mtext(x$ylab,           # y axis label
                   side  = 2,
-                  line  = 1,
+                  line  = 3,
                   cex   = cex.adj,
                   outer = TRUE)
   graphics::mtext(x$title,          # main title
