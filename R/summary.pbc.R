cc300d92b0c2ed9f0ef8bba39d242a2ccdfadcb6
Jacob Anhøj <jacob@anhoej.net>
2025-03-25 09:11:30 +0100

    Fix n.useful from summary().

--- R/summary.pbc.R
+++ R/summary.pbc.R
@@ -10,17 +10,17 @@
 #' p <- pbc(rnorm(12), chart = 'i', plot = FALSE)
 #' summary(p)
 summary.pbc <- function(object, ...) {
-  d      <- object$data
-  freeze <- object$freeze
+  d       <- object$data
+  freeze  <- object$freeze
+  exclude <- object$exclude
 
   if (is.null(freeze))
     freeze <- NA
-
   d <- split(d, d$facet)
   d <- lapply(d, function(x) {
     data.frame(facet        = x$facet[1],
                n            = nrow(x),
-               n.useful     = sum(x$useful),
+               n.useful     = sum(x$useful, na.rm = TRUE) - length(exclude),
                freeze       = freeze,
                avg_lcl      = mean(x$lcl, na.rm = TRUE),
                cl           = mean(x$cl, na.rm = TRUE),
@@ -28,5 +28,5 @@ summary.pbc <- function(object, ...) {
                sigma.signal = sum(x$sigma.signal, na.rm = TRUE),
                runs.signal  = as.integer(any(x$runs.signal)))
   })
-  do.call(rbind, c(d, make.row.names = F))
+  do.call(rbind, c(d, make.row.names = FALSE))
 }
